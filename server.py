0416bc00a9e70a7f8421a85289dc7d90483c1866
Christian Kabulo <christianroykabulo@gmail.com>
2019-04-03 12:36:31 -0400

    Update server.py

--- server.py
+++ server.py
@@ -35,5 +35,5 @@ MyServer.protocol_version = 'HTTP/1.0'
 httpd = server(server_address, MyServer)
 
 sa = httpd.socket.getsockname()
-print 'Serveur actif a ', sa[0], 'port', sa[1]
+print ('Serveur actif a ' + sa[0] + ' port ' + str(sa[1]))
 httpd.serve_forever()
